8e6371b07aa4dba81f44cc126bb802058089fa2c
MikitaDavydau <daff.nik@gmail.com>
2013-01-14 17:06:54 +0300

    Try use AutoComplete

--- src/com/example/issoft/Browser/BrowserMapActivity.java
+++ src/com/example/issoft/Browser/BrowserMapActivity.java
@@ -1,19 +1,34 @@
 package com.example.issoft.Browser;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.text.method.CharacterPickerDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
+import android.widget.*;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.maps.GeoPoint;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * User: nikitadavydov
@@ -27,18 +42,24 @@ public class BrowserMapActivity extends FragmentActivity {
     static final LatLng MINSK = new LatLng(53.9, 27.566667);
     static final LatLng NOVOPOLOTSK = new LatLng(55.5333, 28.6500);
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_widget);
 
-        initMapFragment();
+        initViews();
         initAutoComplete();
-        initButton();
 
+        setMarker(MINSK, "Minsk", "");
     }
 
-    private void initButton() {
+    private void makeToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private void initViews() {
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         saveButton = (Button) findViewById(R.id.saveLocationButton);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
@@ -50,14 +71,18 @@ public class BrowserMapActivity extends FragmentActivity {
         });
     }
 
-
-    private void initMapFragment() {
-        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-
-        Marker minsk = mMap.addMarker(new MarkerOptions()
-                .position(MINSK)
-                .title("Minsk")
-                .snippet("You are here"));
+    private void setMarker(LatLng position, String title, String snippet) {
+        Marker marker;
+        if (snippet != null) {
+            marker = mMap.addMarker(new MarkerOptions()
+                    .position(position)
+                    .title(title)
+                    .snippet(snippet));
+        } else {
+            marker = mMap.addMarker(new MarkerOptions()
+                    .position(position)
+                    .title(title));
+        }
     }
 
     private void initAutoComplete() {
@@ -65,17 +90,50 @@ public class BrowserMapActivity extends FragmentActivity {
         textView = (AutoCompleteTextView) findViewById(R.id.cityAutoCompleteLocation);
         // Get the string array
         String[] countries = getResources().getStringArray(R.array.city_array);
+
         // Create the adapter and set it to the AutoCompleteTextView
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item, countries);
+        adapter.setNotifyOnChange(true);
         textView.setAdapter(adapter);
+
+        textView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                geoCoder(editable.toString());
+            }
+        });
 
         textView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String cityName = (String) adapterView.getItemAtPosition(i);
-                if (cityName.equals("MINSK")) mMap.animateCamera(CameraUpdateFactory.newLatLng(MINSK));
-                else if (cityName.equals("NOVOPOLOTSK")) mMap.animateCamera(CameraUpdateFactory.newLatLng(NOVOPOLOTSK));
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String str = (String) adapterView.getItemAtPosition(position);
+                makeToast(str);
             }
         });
+    }
+
+    private List<Address> geoCoder(String locationName) {
+        Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geoCoder.getFromLocationName(locationName, 5);
+            String add = "";
+            if (addresses.size() > 0) {
+                return addresses;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
