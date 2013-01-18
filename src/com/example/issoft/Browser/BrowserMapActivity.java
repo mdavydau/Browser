1c8fadd809988b9b60cacab1ca9f197b841f0230
MikitaDavydau <daff.nik@gmail.com>
2013-01-18 18:09:57 +0300

    parser problems

--- src/com/example/issoft/Browser/BrowserMapActivity.java
+++ src/com/example/issoft/Browser/BrowserMapActivity.java
@@ -1,6 +1,11 @@
 package com.example.issoft.Browser;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -8,19 +13,31 @@ import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
+import com.example.issoft.Browser.Route.GoogleParser;
+import com.example.issoft.Browser.Route.Parser;
+import com.example.issoft.Browser.Route.Route;
+import com.example.issoft.Browser.Route.RouteOverlay;
 import com.example.issoft.Browser.Util.CustomAddress;
 import com.example.issoft.Browser.Util.CustomAddressArrayAdapter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.maps.GeoPoint;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -35,10 +52,22 @@ import static com.example.issoft.Browser.Util.Constants.MINSK;
  */
 public class BrowserMapActivity extends FragmentActivity {
 
+    private static final String BROWSER_MAP_ACTIVITY = BrowserMapActivity.class.getName();
+    private static final String CURRENT_LOCATION = "CURRENT_LOCATION";
+    private final int CHOOSE_POINT_DIALOG = 0;
+    private final int START_POINT_DIALOG = 1;
+    private final int END_POINT_DIALOG = 2;
+
     private GoogleMap mMap;
+    private GoogleMapOptions googleMapOptions;
     private AutoCompleteTextView textView;
     private Button saveButton;
-    private CustomAddress customAddress;
+    private CustomAddress currentCustomAddress;
+    private AlertDialog.Builder builder;
+
+    private LatLng startLatlng;
+    private LatLng finishLatlng;
+    private LatLng currentLatlng;
 
     private CustomAddressArrayAdapter<CustomAddress> customAddressArrayAdapter;
 
@@ -54,8 +83,25 @@ public class BrowserMapActivity extends FragmentActivity {
 
         initViews();
         initAutoComplete();
-
         makeMarker(MINSK, "Minsk", "");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        /**
+         * bad realization, but worked
+         * @param currentLocation
+         * take from zero-search place
+         * */
+        SharedPreferences settings = getSharedPreferences(BROWSER_MAP_ACTIVITY, 0);
+        String[] currentLocation;
+        try {
+            currentLocation = settings.getString(CURRENT_LOCATION, "").split(",");
+            findCurrentLocationByFeatureNameAndCountryName(currentLocation[0], currentLocation[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Log.e(BROWSER_MAP_ACTIVITY, e.toString());
+        }
+
     }
 
     private void initViews() {
@@ -69,7 +115,11 @@ public class BrowserMapActivity extends FragmentActivity {
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                //todo: set new pointer
+                currentLatlng = latLng;
+
+                if (finishLatlng == null || startLatlng == null) showDialog(CHOOSE_POINT_DIALOG);
+                if (finishLatlng != null && startLatlng != null) route();
+                makeToast("lat:" + latLng.latitude + " lng: " + latLng.longitude);
             }
         });
 
@@ -78,8 +128,61 @@ public class BrowserMapActivity extends FragmentActivity {
             @Override
             public void onClick(View view) {
                 saveCurrentLocationToMenu();
+
+                SharedPreferences settings = getSharedPreferences(BROWSER_MAP_ACTIVITY, 0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putString(CURRENT_LOCATION, currentCustomAddress.getAddress().getFeatureName() + ", " + currentCustomAddress.getAddress().getCountryName());
+                editor.commit();
             }
         });
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case CHOOSE_POINT_DIALOG:
+                final String[] dialogItems = {"Start point", "End point"};
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add route points");
+                builder.setItems(dialogItems, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (dialogItems[item].equals("Start point")) {
+                            startLatlng = currentLatlng;
+                        } else if (dialogItems[item].equals("End point")) {
+                            finishLatlng = currentLatlng;
+                        }
+                    }
+                });
+                return builder.create();
+            case START_POINT_DIALOG:
+                final String[] start = {"Start point"};
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add route points");
+                builder.setItems(start, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (start[item].equals("Start point")) {
+                            startLatlng = currentLatlng;
+                        }
+                    }
+                });
+                return builder.create();
+            case END_POINT_DIALOG:
+                final String[] finish = {"End point"};
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("Add route points");
+                builder.setItems(finish, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        if (finish[item].equals("End point")) {
+                            finishLatlng = currentLatlng;
+                        }
+                    }
+                });
+                return builder.create();
+            default:
+                return null;
+        }
     }
 
     @Override
@@ -101,7 +204,7 @@ public class BrowserMapActivity extends FragmentActivity {
 
     private void saveCurrentLocationToMenu() {
         //todo: before close program - save this stuff to DB
-        if (!menuSavedCustomAddress.contains(customAddress)) menuSavedCustomAddress.add(customAddress);
+        if (!menuSavedCustomAddress.contains(currentCustomAddress)) menuSavedCustomAddress.add(currentCustomAddress);
     }
 
     private void initAutoComplete() {
@@ -130,9 +233,9 @@ public class BrowserMapActivity extends FragmentActivity {
         textView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                customAddress = (CustomAddress) adapterView.getItemAtPosition(position);
-                goToCurrentPosition(customAddress.getAddress());
-                textView.setText(customAddress.getAddress().getFeatureName() + ", " + customAddress.getAddress().getCountryName());
+                currentCustomAddress = (CustomAddress) adapterView.getItemAtPosition(position);
+                goToCurrentPosition(currentCustomAddress.getAddress());
+                textView.setText(currentCustomAddress.getAddress().getFeatureName() + ", " + currentCustomAddress.getAddress().getCountryName());
                 hideKeyboardAndRequestFocus();
             }
         });
@@ -164,7 +267,7 @@ public class BrowserMapActivity extends FragmentActivity {
         LatLng currentLatLng = new LatLng(address.getLatitude(), address.getLongitude());
         mMap.animateCamera(CameraUpdateFactory.newLatLng(currentLatLng));
 
-        makeMarker(currentLatLng, address.getFeatureName(), address.getCountryName() + "\n" + "(" + address.getLatitude() + address.getLongitude() + ")");
+        makeMarker(currentLatLng, address.getFeatureName(), address.getCountryName() + " (" + address.getLatitude() + ", " + address.getLongitude() + ")");
     }
 
     private void geoCoder(final String locationName) {
@@ -192,9 +295,59 @@ public class BrowserMapActivity extends FragmentActivity {
         handler.postDelayed(runnable, 1000);
     }
 
+    private void findCurrentLocationByFeatureNameAndCountryName(final String feature, final String country) {
+        final Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                List<Address> addresses;
+                Address currentAddress;
+                try {
+                    addresses = geoCoder.getFromLocationName(feature, 5);
+                    if (addresses.size() > 1) {
+                        for (Address address : addresses) {
+                            currentAddress = new CustomAddress(address);
+                            if (currentAddress.getCountryName().equals(country))
+                                goToCurrentPosition(currentAddress);
+                        }
+                    } else if (addresses.size() == 1) {
+                        goToCurrentPosition(addresses.get(0));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1000);
+    }
+
     private void hideKeyboardAndRequestFocus() {
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(textView.getWindowToken(), 0);
         saveButton.requestFocus();
+    }
+
+    private void route() {
+        Route route = directions(startLatlng, finishLatlng);
+        RouteOverlay routeOverlay = new RouteOverlay(route, Color.BLUE);
+    }
+
+    private Route directions(final LatLng start, final LatLng dest) {
+        Parser parser;
+        //https://developers.google.com/maps/documentation/directions/#JSON <- get api
+        String jsonURL = "http://maps.googleapis.com/maps/api/directions/json?";
+        final StringBuffer sBuf = new StringBuffer(jsonURL);
+        sBuf.append("origin=");
+        sBuf.append(start.latitude);
+        sBuf.append(',');
+        sBuf.append(start.longitude);
+        sBuf.append("&destination=");
+        sBuf.append(dest.latitude);
+        sBuf.append(',');
+        sBuf.append(dest.longitude);
+        sBuf.append("&sensor=true&mode=driving");
+        parser = new GoogleParser(sBuf.toString());
+        Route r = parser.parse();
+        return r;
     }
 }
---
+++ src/com/example/issoft/Browser/Route/Parser.java
@@ -0,0 +1,9 @@
+package com.example.issoft.Browser.Route;
+
+/**
+ * User: nikitadavydov
+ * Date: 1/18/13
+ */
+public interface Parser {
+    public Route parse();
+}
